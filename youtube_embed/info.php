c4d98dbe2732ee68c0789382e63780712f44df6e
Ivan A. Shvindin <ivan.shvindin@gmail.com>
2012-12-11 22:57:21 +0400

    v0.1

---
+++ youtube_embed/info.php
@@ -0,0 +1,12 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+
+$info = array(
+	'name' => 'Youtube Embed',
+	'description' => 'Плагин удобной вставки видео с youtube',
+	'version' => '0.1',
+	'author' => 'Иван Швиндин',
+	'plugin_url' => 'http://shvind.ru/',
+	'author_url' => 'http://shvind.ru/',
+	'group' => 'template'
+);
+
